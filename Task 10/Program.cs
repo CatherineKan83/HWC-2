295f0c10ac7625f39c18285d03c702f2fba64203
Catherine <kankatya@bk.ru>
2022-08-16 04:41:31 +0500

    Added Task10

---
+++ Task 10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число: ");
+
+int n = int.Parse(Console.ReadLine());
+
+int FirstDigit = n/100;
+int SecondDigit = (n - (FirstDigit*100 + n%10))/10;
+Console.WriteLine("Второе число вашего числа: " + SecondDigit);
+
