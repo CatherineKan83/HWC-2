75b6d4b98551d4abf3296b63ae91a48392284d15
Catherine <kankatya@bk.ru>
2022-08-16 08:32:55 +0500

    Added Task 15

---
+++ Task 15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число и, возможно, я Вас обрадую.");
+int D = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("~>>" + D);
+int i = 7;
+if ((D%i ==6) | (D%i==0))
+{
+    Console.WriteLine("Да!!! Выходной! Йэээ, живём!");
+}
+else
+{
+    Console.WriteLine("Нет. Сожалею,но это будний день...");
+}
